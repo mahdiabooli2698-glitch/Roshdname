848936133553e585c0a010dba107d9720687f523
mahdiabooli2698-glitch <mahdiabooli2698@gmail.com>
2026-09-18 12:13:49 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE = 'roshdnameh-v6';
+const CACHE = 'roshdnameh-v7';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/icon-maskable-192.png',
-  './icons/icon-maskable-512.png'
+  './icon-192.png',
+  './icon-512.png',
+  './icon-maskable-192.png',
+  './icon-maskable-512.png'
 ];
 
 self.addEventListener('install', (e) => {
